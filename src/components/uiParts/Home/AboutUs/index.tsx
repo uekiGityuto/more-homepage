611013af9919befbe9c057ed3d2c98acb16eed7e
Yuto Ueki <ueki.yuto.bz@gmail.com>
2024-06-16 02:51:52 +0900

    add resume

--- src/components/uiParts/Home/AboutUs/index.tsx
+++ src/components/uiParts/Home/AboutUs/index.tsx
@@ -1,4 +1,5 @@
 import { H2, H3 } from "@/components/atoms/Typography";
+import Link from "next/link";
 
 export const AboutUs = () => {
   return (
@@ -11,6 +12,18 @@ export const AboutUs = () => {
           その後、2021年に個人事業主として独立してからは、金融機関だけでなく、メガベンチャーやスタートアップなど幅広いお客様のシステム開発を支援。
           <br />
           2024年にMore株式会社を設立。
+        </p>
+        <p>
+          代表者の詳細な経歴は
+          <a
+            href="https://morecoltd.notion.site/8e8d4a1c81b44cfbba438ff40f0bb1c6"
+            className="text-link-100"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            こちらのレジュメ
+          </a>
+          を参照。
         </p>
       </div>
     </section>
