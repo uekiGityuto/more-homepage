99004069928f45b81f309db753aa3617de0d9cd9
Yuto Ueki <ueki.yuto.bz@gmail.com>
2024-06-15 16:59:30 +0900

    fix footer

--- src/components/layout/Footer/index.tsx
+++ src/components/layout/Footer/index.tsx
@@ -1,15 +1,21 @@
 import Link from "next/link";
 
-export const Footer = () => (
-  <nav className="w-full px-6 md:px-80 py-4 bg-primary-10 flex gap-5">
-    <Link href="/" className="underline text-link-100">
-      トップ
-    </Link>
-    <Link href="/securityPolicy" className="underline text-link-100">
-      セキュリティーポリシー
-    </Link>
-    <Link href="/privacyPolicy" className="underline text-link-100">
-      プライバシーポリシー
-    </Link>
-  </nav>
-);
+export const Footer = () => {
+  const currentYear = new Date().getFullYear();
+  return (
+    <footer className="w-full px-6 md:px-80 py-4 bg-primary-10 flex justify-between gap-4 flex-col md:flex-row">
+      <nav className="flex gap-4 flex-col md:flex-row">
+        <Link href="/" className="underline text-link-100">
+          トップ
+        </Link>
+        <Link href="/securityPolicy" className="underline text-link-100">
+          セキュリティーポリシー
+        </Link>
+        <Link href="/privacyPolicy" className="underline text-link-100">
+          プライバシーポリシー
+        </Link>
+      </nav>
+      <p>© {currentYear} More株式会社</p>
+    </footer>
+  );
+};
