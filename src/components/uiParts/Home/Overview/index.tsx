646d5030044c5a93fc04faedb502003d7aefed85
Yuto Ueki <ueki.yuto.bz@gmail.com>
2024-06-15 20:03:44 +0900

    fix to add contact on overview

--- src/components/uiParts/Home/Overview/index.tsx
+++ src/components/uiParts/Home/Overview/index.tsx
@@ -1,11 +1,11 @@
 import { H2 } from "@/components/atoms/Typography";
 
-const DATA = [
+const DATA: { label: string; value: string }[] = [
   { label: "企業名", value: "More株式会社" },
   { label: "設立", value: "2024年3月6日" },
   { label: "資本金", value: "100万円" },
   { label: "代表者", value: "山川宥登" },
-  // TODO: 問い合わせ先の情報を追加する
+  { label: "問い合わせ先", value: "more.contact.morecoltd@gmail.com" },
 ];
 
 export const Overview = () => (
